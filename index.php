99cea10cbcc9acaec28aab4564092fb09d056f1e
stormbreakerbg <georgyangelov@gmail.com>
2011-06-28 21:23:16 +0300

    Debugване

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php
 include 'AStar2.php';
 
-$max_x = 50;
-$max_y = 50;
+$max_x = 144;
+$max_y = 144;
 
 $map = AStar::getRandomMap($max_x, $max_y);
-$map[45][7] = 0;
+$map[125][9] = 0;
 $map[1][2] = 0;
 
-$path = new AStar($map, 1, 2, 45, 7);
+$path = new AStar($map, 1, 2, 125, 9);
 
+    $mtime = microtime(); 
+    $mtime = explode(" ",$mtime); 
+    $mtime = $mtime[1] + $mtime[0];
+    $starttime = $mtime; 
+    
 $path->findShortestPath();
+
+$mtime = microtime(); 
+   $mtime = explode(" ",$mtime); 
+   $mtime = $mtime[1] + $mtime[0]; 
+   $endtime = $mtime; 
+   $totaltime = ($endtime - $starttime); 
+   echo "The path was found in ".(round($totaltime, 3) * 1000)." milliseconds"; 
 
 echo '<table border="1"><tr>';
 for ($y=0; $y<$max_y; $y++)
